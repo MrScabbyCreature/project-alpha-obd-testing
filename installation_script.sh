673da975a3949aba6eb98edaa911d7a20cdef0c7
Abhor <abhorg@iitbhilai.ac.in>
2019-12-30 22:59:29 +0530

    asd

--- installation_script.sh
+++ installation_script.sh
@@ -5,9 +5,12 @@ wget http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet50
 tar xvzf faster_rcnn_resnet50_coco_2018_01_28.tar.gz
 
 #add directory and change position and delete
-mkdir graph
-mv faster_rcnn_resnet50_coco_2018_01_28/frozen_inference_graph.pb graph/
+mkdir code/graph
+mv faster_rcnn_resnet50_coco_2018_01_28/frozen_inference_graph.pb code/graph/
 rm faster_rcnn_resnet50_coco_2018_01_28.tar.gz
 rm -r faster_rcnn_resnet50_coco_2018_01_28
 
-#download images
+#do images
+cd code
+tar xvzf images.tar.gz
+rm images.tar.gz
